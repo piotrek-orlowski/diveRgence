7a12a738c05bf4967907d722575e34cc6be1a7f9
Piotrek Orłowski <piotrek.orlowski@gmail.com>
2016-06-03 16:43:43 +0000

    clt monte carlo now in loop, drastically smaller memory requirements

--- src/generalizedCLT.cpp
+++ src/generalizedCLT.cpp
@@ -10,61 +10,73 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 arma::vec mcCltInference(arma::vec& rdivDerivX, arma::vec& rdivDerivZ, double rdivCont, arma::vec& spotVolPlus, arma::vec& spotVolMinus, int nSampl){
   
-// Matrix dimensions
+  // Matrix dimensions
   int nObs = rdivDerivX.n_elem;
   
-// Create matrices of random numbers;
-  arma::mat uPlus(nObs, nSampl, arma::fill::randn);
-  arma::mat uMinus(nObs, nSampl, arma::fill::randn);
-  arma::mat kpp(nObs, nSampl, arma::fill::randu);
-  arma::mat oneLessKpp(nObs, nSampl);
-  arma::vec uCont(nSampl, arma::fill::randn);
+  // Create limiting sum vector
+  arma::vec limitVarSum(nSampl);
   
-  oneLessKpp.fill(1.0);
-  oneLessKpp -= kpp;
-  
-// Create matrices filled with inputs
-  arma::mat mat_rdivDerivX(nObs, nSampl);
-  arma::mat mat_rdivDerivZ(nObs, nSampl);
-  arma::mat mat_spotVolPlus(nObs, nSampl);
-  arma::mat mat_spotVolMinus(nObs, nSampl);
-  arma::vec uContVol(nSampl);
-  
-// Create limiting variable matrix
-  arma::mat mat_limitVar(nObs, nSampl);
-  
-// Create limiting sum vector
-  arma::vec vec_limitVarSum(nSampl);
-  
-// Set RNG with R
+  // Set RNG with R
   RNGScope rngScope;
   
-// Start filling -- first the square root of cont variance
-  uContVol.fill(pow(rdivCont,0.5));
-
-// Take square roots of spot volatilities
+  // Start filling -- first the square root of cont variance
+  // arma::vec uContVol(1);
+  // uContVol.fill(pow(rdivCont,0.5));
+  rdivCont = pow(rdivCont,0.5);
+    
+  // Take square roots of spot volatilities
   spotVolPlus = arma::sqrt(spotVolPlus);
-  spotVolMinus = arma::sqrt(spotVolMinus);
-
-// Write data vectors to large matrices
-  for(int nn=0; nn < nSampl; nn++){
-    mat_rdivDerivX.col(nn) = rdivDerivX.col(0);
-    mat_rdivDerivZ.col(nn) = rdivDerivZ.col(0);
-    mat_spotVolPlus.col(nn) = spotVolPlus.col(0);
-    mat_spotVolMinus.col(nn) = spotVolMinus.col(0);
+  spotVolMinus = arma::sqrt(spotVolMinus);  
+    
+  for(int nIter = 0; nIter < nSampl; nIter++){
+    // Create matrices of random numbers;
+    arma::vec uPlus(nObs, arma::fill::randn);
+    arma::vec uMinus(nObs, arma::fill::randn);
+    arma::vec kpp(nObs, arma::fill::randu);
+    arma::vec oneLessKpp(nObs);
+    arma::vec uCont(1L, arma::fill::randn);
+    
+    oneLessKpp.fill(1.0);
+    oneLessKpp -= kpp;  
+    
+    // Create limiting variable matrix
+    arma::vec limitVar(nObs);
+    
+    // Calculate the limiting variable
+    limitVar = uPlus % arma::sqrt(kpp) % spotVolMinus % rdivDerivX;
+    limitVar += uMinus % arma::sqrt(oneLessKpp) % spotVolPlus % rdivDerivX;
+    limitVar -= uMinus % arma::sqrt(kpp) % spotVolMinus % rdivDerivZ;
+    
+    limitVarSum(nIter) = arma::sum(limitVar);
+    // add variation coming from the cont part;
+    limitVarSum(nIter) += rdivCont * uCont(0);    
   }
 
-// Calculate the limiting variable
-  mat_limitVar = uPlus % arma::sqrt(kpp) % mat_spotVolMinus % mat_rdivDerivX;
-  mat_limitVar += uMinus % arma::sqrt(oneLessKpp) % mat_spotVolPlus % mat_rdivDerivX;
-  mat_limitVar -= uMinus % arma::sqrt(kpp) % mat_spotVolMinus % mat_rdivDerivZ;
-  
-// Sum the matrix for each MC sample;
-  vec_limitVarSum = sum(mat_limitVar, 0).t();
+  // // Create matrices filled with inputs
+//   arma::mat mat_rdivDerivX(nObs, nSampl);
+//   arma::mat mat_rdivDerivZ(nObs, nSampl);
+//   arma::mat mat_spotVolPlus(nObs, nSampl);
+//   arma::mat mat_spotVolMinus(nObs, nSampl);
 
-// add variation coming from the cont part;
-  vec_limitVarSum += uContVol % uCont;
+// // Write data vectors to large matrices
+//   for(int nn=0; nn < nSampl; nn++){
+//     mat_rdivDerivX.col(nn) = rdivDerivX.col(0);
+//     mat_rdivDerivZ.col(nn) = rdivDerivZ.col(0);
+//     mat_spotVolPlus.col(nn) = spotVolPlus.col(0);
+//     mat_spotVolMinus.col(nn) = spotVolMinus.col(0);
+//   }
+// 
+// // Calculate the limiting variable
+//   mat_limitVar = uPlus % arma::sqrt(kpp) % mat_spotVolMinus % mat_rdivDerivX;
+//   mat_limitVar += uMinus % arma::sqrt(oneLessKpp) % mat_spotVolPlus % mat_rdivDerivX;
+//   mat_limitVar -= uMinus % arma::sqrt(kpp) % mat_spotVolMinus % mat_rdivDerivZ;
+//   
+// // Sum the matrix for each MC sample;
+//   vec_limitVarSum = sum(mat_limitVar, 0).t();
+
+// // add variation coming from the cont part;
+//   vec_limitVarSum += uContVol % uCont;
   
 // RETURN
-  return(vec_limitVarSum);
+  return(limitVarSum);
 }
